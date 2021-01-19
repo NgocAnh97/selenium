afbf63ef3e0b3fbecd58c02e9f8cd46da47f642e
Giang Dinh <thanhgiang.hcmus@gmail.com>
2021-01-20 02:59:22 +0700

    fix noSuchElementLocated

--- src/specs/login_test.js
+++ src/specs/login_test.js
@@ -9,55 +9,47 @@ const assert = require('assert');
 
 // (async () => {
 describe("Login Reputa automation test", () => {
+  let driver = new Builder().forBrowser('chrome').build();
+  it("Test case", async () => {
+    try {
+      await driver.manage().deleteAllCookies();
+      await driver.get("https://reputa.vn/");
+      const btnLogin = await driver.findElement(By.xpath("//span[@class='navbar-text']"));
+      await btnLogin.click();
+      const userName = await driver.findElement(By.xpath("//input[@type='text']"));
+      await userName.sendKeys("tesvtel54@yopmail.com");
+      await driver.getTitle().then(function (title) {
+        console.log("The title is: " + title)
+      });
 
-    // it('should complete this test', function(done) {
-    //     return new Promise(function(resolve) {
-    //         resolve();
-    //     }).then(done);
-    // });
-    // beforeEach(async (done)=>{
-    //     // return true;
-    //     let driver = await new Builder().forBrowser('chrome').build();
-    // })
-    let driver = new Builder().forBrowser('chrome').build();
-    it("Test case", async () => {
-        try {
-            driver.manage().deleteAllCookies();
-            await driver.get("https://reputa.vn/");
-            const btnLogin = await driver.findElement(By.xpath("//span[@class='navbar-text']"));
-            await btnLogin.click();
-            const userName = await driver.findElement(By.xpath("//input[@type='text']"));
-            await userName.sendKeys("tesvtel54@yopmail.com");
-            await driver.getTitle().then(function (title) {
-                console.log("The title is: " + title)
-            });
-            const password = await driver.findElement(By.xpath("//input[@type='password']")).sendKeys("123456aA@@");
-            const btnNext = await driver.findElement(By.xpath("//button[@type='submit']")).click();
+      await driver.findElement(By.xpath("//input[@type='password']")).sendKeys("123456aA@@");
+      await driver.findElement(By.xpath("//button[@type='submit']")).click();
 
-            // await assert(driver.getTitle,' Đăng nhập - Reputa');
+      //Promise: then
+      await driver.getTitle().then(function (title) {
+        console.log("The title of web is: " + title)
+      });
+      await driver.wait(until.titleIs("Reputa"), 3000);
 
-            //Promise: then
-            await driver.getTitle().then(function (title) {
-                console.log("The title of web is: " + title)
-            });
-            await driver.wait(until.titleIs("Reputa"), 3000);
+      await driver.wait(until.elementLocated(By.xpath("//div[@class='nav-item alert-header']")), 10000);
+      await driver.findElement(By.xpath("//div[@class='nav-item alert-header']/a")).then(() => {
+        console.log("Kiem thay button alert ");
+      }).catch((err) => {
+        console.log(err);
+      });
 
-            await driver.findElement(By.xpath("//div[@class='nav-item alert-header/a']")).then(function (done) {
-                console.log("Kiem thay button alert");
-            });
+      // const btnNoti = await driver.findElement(By.xpath("//div[@class='nav-item alert-header/a']")).click();
+      const btnTopic = await driver.findElement(By.xpath("//div[@class='top-nav']/div[5]"));
+      await btnTopic.click();
+      const btnSettingTopic = await driver.findElement(By.xpath("//div[@class='action-part']/div[3]")).click();
 
-            // const btnNoti = await driver.findElement(By.xpath("//div[@class='nav-item alert-header/a']")).click();
-            const btnTopic = await driver.findElement(By.xpath("//div[@class='top-nav']/div[5]"));
-            await btnTopic.click();
-            const btnSettingTopic = await driver.findElement(By.xpath("//div[@class='action-part']/div[3]")).click();
-
-        } catch (e) {
-            console.log("Error is: " + e);
-            // handleFailure(e, driver);
-        } finally {
-            assert.ok(true);
-            await driver.quit();
-        }
-    })
+    } catch (e) {
+      console.log("Error is: " + e);
+      // handleFailure(e, driver);
+    } finally {
+      assert.ok(true);
+      await driver.quit();
+    }
+  })
 })
 
