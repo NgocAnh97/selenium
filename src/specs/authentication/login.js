8d998021d19432aa104bfd394c9a7e89b987ba17
AnhDTN <anhdtn15@cyberspace.vn>
2021-02-26 18:48:55 +0700

    excel

--- src/specs/authentication/login.js
+++ src/specs/authentication/login.js
@@ -6,7 +6,7 @@ import pkg2 from "chai";
 const { assert } = pkg2;
 const { By, until, titleContains } = pkg;
 import ExcelJS from 'exceljs';
-import array from './readExcelFile.js'
+import array from './readExcelFile.js';
 
 
 describe("~~~Reputa Automation Authentication test suite~~~", () => {
@@ -18,7 +18,7 @@ describe("~~~Reputa Automation Authentication test suite~~~", () => {
   var result;
   const workbook = new ExcelJS.Workbook();
   var worksheet = workbook.getWorksheet("Sheet1");
-
+  //
   it("should login success", async () => {
     for (let index = 0; index < array.length; index++) {
       result = await axios.post(`${config.apiCollection.login}`, {
@@ -27,19 +27,23 @@ describe("~~~Reputa Automation Authentication test suite~~~", () => {
       });
       if (result && result.data) {
         console.log(`Message: ${result.data.message}`);
-        // try {
-        //   assert.equal(result.data.code, 10);
-        // } catch (AssertionError) {
-        //   assert.equal(result.data.code, 21);
-        // }
-        var cell = worksheet.getCell(3);
-        console.log(cell);
-        cell.getRow(index).value = result.data.code;
-        cell.commit();
-        workbook.xlsx.writeFile("D:/reputa_automation/src/data/loginCase.xlsx");
+        try {
+          assert.equal(result.data.code, 10);
+        } catch (AssertionError) {
+          assert.equal(result.data.code, 21);
+        }
       }
     }
+    for (var i = 2; i <= worksheet.rowCount; i++) { 
+      var cell = worksheet.getCell(3);
+      console.log(cell);
+      cell.getRow(i).value = result.data.code;
+      cell.commit();
+      workbook.xlsx.writeFile("D:/reputa_automation/src/data/loginCase.xlsx");
+    }
   })
+
+  // })
 
   // it("Verify the Web heading title text is displayed", async () => {
   //   await driver.manage().deleteAllCookies();
@@ -51,53 +55,11 @@ describe("~~~Reputa Automation Authentication test suite~~~", () => {
   //     }).catch(e => console.log(e))
   // })
 
-  // it("should login successfully", async () => {
-  //   try {
-  //     await driver.manage().deleteAllCookies();
-  //     await driver.get(config.url);
-
-  //     await driver.findElement(By.xpath("//span[@class='navbar-text']")).click();
-  //     await driver.findElement(By.xpath("//input[@type='text']")).sendKeys(config.username);
-  //     await driver.findElement(By.xpath("//input[@type='password']")).sendKeys(config.password);
-  //     await driver.findElement(By.xpath("//button[@type='submit']")).click();
-  // //     function sleep(time) {
-  // //       return new Promise((resolve) => {
-  // //         setTimeout(resolve, time || 1000);
-  // //       });
-  // //     }
-  // driver.sleep(10000);
-  //   driver.findElement(By.xpath('//*[@id="top-nav"]/div[2]/div[4]/div'))
-  //   .then(function(searchTK){
-  //     searchTK.click();
-  //   })
-  // // const btnTK = driver.findElement(By.linkText("Thống kê"));
-  // //     await driver.wait(until.elementIsVisible(btnTK), 20000);
-  // //     await btnTK.getText();
-
-  // //     btnTK.click();
-  // //     await sleep(10000);
-  // //     // 
-  // //     // console.log(btnTK.getText());
-
-  // //     // await driver.wait(until.elementLocated(By.xpath("//div[@class='nav-item alert-header']")), 10000);
-  // //     // const btnNoti = driver.findElement(By.xpath("//ul[contains(@class,'right-menu navbar-nav')]/div[1]/a"));
-  // //     // await driver.findElement(By.xpath("//ul[contains(@class,'right-menu navbar-nav')]/div[1]/a")).then(async () => {
-  // //     //   await driver.findElement(By.xpath("//ul[contains(@class,'right-menu navbar-nav')]/div[1]/a")).click();
-  // //     //   await driver.wait(until.elementIsEnabled(btnNoti), 20000);
-  // //     //   await driver.findElement(By.xpath("//a[contains(@class,'dropdown-item noti-read-more')]")).click();
-  // //     // }).catch((err) => {
-  // //     //   console.log(err);
-  // //     // })
-  //   } catch (e) {
-  //     console.log("Loiiiii: " + e.message);
-  //     console.log("Stacktrace: " + e.stack);
-  //   }
-  // })
-
   // after(async () => driver && driver.quit());
   after(async () => {
     if (driver !== undefined) {
       await driver.quit();
     }
   })
+
 })
